68f2fd4d2c69981a0e77a58f95ddf308ec0fa5ce
asubbotin <asubbotin@snaplogic.com>
2023-08-28 16:57:34 -0400

    new metrics

--- src/main/kotlin/krafana/app/ApiStatsDashboard.kt
+++ src/main/kotlin/krafana/app/ApiStatsDashboard.kt
@@ -10,24 +10,68 @@ fun apiStatsDashboard(dataSource: DataSource) = dashboard {
     refresh = 10.s
     time = (now - 15.m)..now
     with(dataSource) {
-        timeseries {
-            title = "Telemetry request count"
-            query {
-                expr = telemetryMetricPusherCountTotal
+        row("Successes") {
+            timeseries {
+                title = "Telemetry request count"
+                query {
+                    expr = telemetryMetricPusherCountTotal.ideltaInterval()
+                }
+            }
+            timeseries {
+                title = "Telemetry request duration"
+                measure = Measure.ms
+                query {
+                    expr = telemetryMetricPusherDuration99pct
+                }
+            }
+            timeseries {
+                title = "Telemetry request size"
+                measure = Measure.bytes
+                query {
+                    expr = telemetryMetricPusherSize99pct
+                }
+            }
+            timeseries {
+                title = "Telemetry request size"
+                measure = Measure.bytes
+                query {
+                    expr = telemetryMetricPusherDatapoints99pct
+                }
             }
         }
-        timeseries {
-            title = "Telemetry request duration"
-            measure = Measure.ms
-            query {
-                expr = telemetryMetricPusherDuration99pct
+        row("Failures") {
+            timeseries {
+                title = "Telemetry request count"
+                query {
+                    expr = telemetryMetricPusherCountFailedTotal.ideltaInterval()
+                }
             }
-        }
-        timeseries {
-            title = "Telemetry request size"
-            measure = Measure.bytes
-            query {
-                expr = telemetryMetricPusherSize99pct
+            timeseries {
+                title = "Telemetry request exceptions"
+                query {
+                    expr = telemetryMetricPusherExceptionTotal.ideltaInterval()
+                }
+            }
+            timeseries {
+                title = "Telemetry request duration"
+                measure = Measure.ms
+                query {
+                    expr = telemetryMetricPusherFailedDuration99pct
+                }
+            }
+            timeseries {
+                title = "Telemetry request size"
+                measure = Measure.bytes
+                query {
+                    expr = telemetryMetricPusherFailedSize99pct
+                }
+            }
+            timeseries {
+                title = "Telemetry request size"
+                measure = Measure.bytes
+                query {
+                    expr = telemetryMetricPusherFailedDatapoints99pct
+                }
             }
         }
     }
